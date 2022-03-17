e3f0d656d83d348248bf5671196d289598574f3e
Cathleen Balantic <cbalantic@gmail.com>
2022-03-17 16:11:04 -0600

    Cleaner arg names & added NVSPL_To_AI example

--- Intro-Demo.R
+++ Intro-Demo.R
@@ -6,6 +6,14 @@
 library(data.table)
 library(NSNSDAcoustics)
 
+# Not sure why these aren't importing even though pkg is built?
+library(tuneR)
+library(ineq)
+library(moments)
+library(NbClust)
+library(svDialogs)
+library(vegan)
+
 # Look at helpfile for Wave_To_NVSPL, which uses PAMGuide
 ?Wave_To_NVSPL
 
@@ -31,11 +39,11 @@ Wave_To_NVSPL(
   data.directory = FALSE,
   test.file = TRUE,
   project = 'testproject',
-  timezone = 'GMT',
-  PAMvers = 'V12_noChunk')
+  timezone = 'GMT')
 
-# see line 1684 + 1729 for example of CB question
-# another question - doc w/ metadata for each column in the output file?
+# PLACES TO CHECK in main Wave_To_NVSPL wrapper function
+# (it's possible the mistakes are in the math of the NPS script, and not in the pamguide code itself?)
+# --> Line 1692 section might be the first place to check?
 
 # Perform Wave_To_NVSPL in batch mode (test.file = FALSE)
 Wave_To_NVSPL(
@@ -43,8 +51,7 @@ Wave_To_NVSPL(
   data.directory = FALSE,
   test.file = FALSE,
   project = 'testproject',
-  timezone = 'GMT',
-  PAMvers = 'V12_noChunk')
+  timezone = 'GMT')
 
 # Verify that NVSPL outputs have been created
 nvspls <- list.files('example-input-directory/NVSPL', full.names = TRUE)
@@ -62,6 +69,57 @@ unlink(x = 'example-input-directory', recursive = TRUE)
 # Then re-source the function change with that browser() call, and the next time
 # you run the function you'll step into the function environment and be able to debug
 
+
+
+
+# NVSPL_To_AI =================================================================
+
+# Create an input and output directory for this example
+dir.create('example-input-directory')
+dir.create('example-output-directory')
+
+# Read in example NVSPL data
+data(exampleNVSPL)
+
+# Write example NVSPL data to example input directory
+for (i in 1:length(exampleNVSPL)) {
+  write.table(x = exampleNVSPL[[i]],
+              file = paste0('example-input-directory/', names(exampleNVSPL)[i]),
+              sep = ',',
+              quote = FALSE)
+}
+
+# Run NVSPL_To_AI to generate acoustic indices csv for example NVSPL files,
+# setting start.at.beginning = FALSE
+NVSPL_To_AI(input.directory = 'example-input-directory',
+            output.directory = 'example-output-directory',
+            project = 'example-project',
+            start.at.beginning = FALSE)
+
+# Run NVSPL_To_AI to generate acoustic indices csv for example NVSPL files,
+# setting start.at.beginning = TRUE
+NVSPL_To_AI(input.directory = 'example-input-directory',
+            output.directory = 'example-output-directory',
+            project = 'example-project',
+            start.at.beginning = TRUE)
+
+
+
+# Read in both files to compare
+start.standard <- read.csv(list.files(path = './example-output-directory/',
+                                      pattern = 'Index_Created',
+                                      full.names = TRUE))
+start.begin <- read.csv(list.files(path = './example-output-directory/',
+                                   pattern = 'Begin_Created',
+                                   full.names = TRUE))
+
+# View a few rows of each file; note the Hr, Min, Sec differences between both options
+start.standard[1:4, ]
+start.begin[1:4, ]
+
+# Delete all temporary example files when finished
+unlink(x = 'example-input-directory', recursive = TRUE)
+unlink(x = 'example-output-directory', recursive = TRUE)
 
 
 
