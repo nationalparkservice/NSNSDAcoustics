b0b9f56bdfd165242b647f80932f075135fa0ba6
cbalantic <cbalantic@gmail.com>
2024-04-01 16:43:50 -0600

    Updated function to be consistent with birdnet_heatmap_time. Added comparable.color.breaks argument and updated helpfile examples.

--- R/birdnet_heatmap.R
+++ R/birdnet_heatmap.R
@@ -1,14 +1,16 @@
 # birdnet_heatmap =================================================================
 
 #' @name birdnet_heatmap
-#' @title Plot heat maps of BirdNET detections
-#' @description Plot heat maps of BirdNET results for a selected species and above a selected confidence threshold through time. See Details.
+#' @title Plot heat maps of BirdNET detections by date
+#' @description Plot heat maps of BirdNET results for a selected species and above a selected confidence threshold by date for multiple or single year data. See Details.
 #' @param data Data.table or data.frame of BirdNET results that a user would like to plot. Generally, this data object may be preceded by a call to \code{\link{add_time_cols}}; all data should come from a single site and the object must contain columns named "locationID" (character), "recordingID" (character), and "dateTimeLocal" (POSIXct). Multiple years of data are allowed in one dataset to enable easy comparison of vocal activity by year at a site.
 #' @param locationID Character input of the locationID for which data should be plotted.
 #' @param common.name Character input of the target species for which data should be plotted.
 #' @param conf.threshold Numeric input of the BirdNET confidence threshold above which data should be plotted. All detections below this confidence threshold will be discarded.
 #' @param dates.sampled Date or character vector of all dates sampled that should be visualized on the heat map. This information is required because your data input may only contain detection data, and not non-detection data (i.e., zeroes). For example, you might have recorded audio on 2021-03-14, but have no BirdNET detections in "data". This will result in an inaccurate visual. Since BirdNET results do not automatically contain non-detection data, it is incumbent on the user to input which dates were sampled.
 #' @param julian.breaks Optional numeric vector of julian date plotting breaks to use on the x axis. If omitted, will be computed automatically. Example inputs: c(140, 160, 180) would graph 3 breaks on the x axis (May 20, June 9, and June 29 for non-leap year data); c(130:160) would graph every single date from May 10 to June 9 on the x axis (for non-leap year data). See also \code{\link{readable_julian_breaks}}. Please start with 1 for the first day of the year rather than 0.
+#' @param tz.local Character Olsen names timezone for local time at the monitoring location (e.g., 'America/Los_angeles').
+#' @param comparable.color.breaks Logical flag for whether to create heat map color breaks based on all species in the input data set or based only on the species of interest in this plot. TRUE means it will be easier to make straightforward comparisons between species, FALSE means activity contrasts within a single species will be easier to see.
 #' @return Heat map of BirdNET detections, where N is the number of detections for common.name above conf.threshold on any given day.
 #' @details
 #' This function was developed by the National Park Service Natural Sounds and Night Skies Division. It is intended to provide exploratory plotting for summarizing and visualizing BirdNET results.
@@ -16,43 +18,65 @@
 #' Function returns a heatmap of BirdNET detections, where N is the number of detections for common.name above conf.threshold on any given day. Note that if you have different sampling efforts on different days, the heat map "as is" may not accurately visualize your target species' vocalization effort. In this case, you may wish to do some preprocessing to your data input to control for differences in sampling effort on different days.
 #'
 #'
-#' @seealso  \code{\link{birdnet_barchart}}
-#' @import data.table ggplot2 lubridate monitoR tuneR
+#' @seealso  \code{\link{birdnet_barchart}} \code{\link{birdnet_heatmap_time}}
+#' @import data.table ggplot2 lubridate monitoR tuneR viridis
 #' @importFrom lubridate month day yday
 #' @export
 #' @examples
 #' \dontrun{
 #'
 #' # Read in example data
-#' data(exampleBarchartData)
+#' data(exampleHeatmapData)
+#' data(exampleDatesSampled)
 #'
-#' # Generally, add_time_cols() may be called as part of preprocessing
-#' # (if not, please ensure data object has columns that include locationID (character),
-#' # recordingID (character), and dateTimeLocal (POSIXct))
-#' dat <- add_time_cols(dt = exampleBarchartData,
-#'                      tz.recorder = 'America/Los_angeles',
-#'                      tz.local = 'America/Los_angeles')
+#' # Ensure your data has an appropriate recordingID column and time columns
+#' dat <- exampleHeatmapData
+#' dat[ ,recordingID := basename(filepath)]
+#' dat <- add_time_cols(
+#'            dt = dat,
+#'            tz.recorder = 'America/Los_angeles',
+#'            tz.local = 'America/Los_angeles'
+#' )
 #'
-#' # Generate a heatmap with user-input julian.breaks
+#'
+#' # Generate a heatmap at Rivendall for Pacific Wren
+#' # Set comparable.color.breaks = FALSE to maximize contrast in a single species map
+#' # Add user-input julian.breaks
 #' birdnet_heatmap(
 #'      data = dat,
 #'      locationID = 'Rivendell',
 #'      common.name = 'Pacific Wren',
 #'      conf.threshold = 0.2,
-#'      dates.sampled = seq.Date(from = as.Date('2021-03-14'),
-#'                               to = as.Date('2021-08-15'),
-#'                               by = 1),
-#'      julian.breaks = seq(from = 70, to = 250, by = 30))
+#'      dates.sampled = exampleDatesSampled,
+#'      julian.breaks = seq(from = 70, to = 250, by = 30),
+#'      comparable.color.breaks = FALSE
+#' )
 #'
-#' # Generate a heatmap with automatic julian.breaks (default = every 14 days)
-#' birdnet_heatmap(
-#'     data = dat,
-#'     locationID = 'Rivendell',
-#'     common.name = 'Pacific Wren',
-#'     conf.threshold = 0.2,
-#'     dates.sampled = seq.Date(from = as.Date('2021-03-14'),
-#'                              to = as.Date('2021-08-15'),
-#'                              by = 1))
+#' # Generate heatmaps for several species with comparable.color.breaks == TRUE
+#' # so that heatmap color scale is conserved for ease of interspecies comparison
+#' sp <- c("Pacific Wren",
+#'         "Pacific-slope Flycatcher",
+#'         "Swainson's Thrush",
+#'         "Wilson's Warbler")
+#'
+#' for (i in 1:length(sp)) {
+#'
+#'  print(paste0('Working on ', sp[i]))
+#'
+#'  g <- birdnet_heatmap(
+#'      data = dat,
+#'      locationID = 'Rivendell',
+#'      common.name = sp[i],
+#'      conf.threshold = 0.2,
+#'      dates.sampled = exampleDatesSampled,
+#'      julian.breaks = seq(from = 70, to = 250, by = 30),
+#'      comparable.color.breaks = TRUE
+#'  )
+#'
+#'  print(g)
+#'
+#' }
+#'
 #' }
 #'
 
@@ -62,7 +86,9 @@ birdnet_heatmap <- function(
     common.name,
     conf.threshold,
     julian.breaks,
-    dates.sampled # vector of character or date data type
+    dates.sampled,
+    tz.local,
+    comparable.color.breaks = FALSE
 ) {
 
   if(missing(dates.sampled)) {
@@ -83,7 +109,6 @@ birdnet_heatmap <- function(
   locid <- locationID
 
   # Create readable julian breaks
-
   if(missing(julian.breaks)) {
     brks <- readable_julian_breaks(
       data = data[locationID == locid], # base off largest breadth of input data?
@@ -108,6 +133,28 @@ birdnet_heatmap <- function(
                               c('julian.date', 'date.lab')])
   }
 
+  # If comparable.color.breaks == TRUE
+  # Create heatmap color breaks based on the whole dataset, not just this species
+  # so that interspecies comparisons are easier visually (i.e., use the same color breaks)
+  if (comparable.color.breaks == TRUE) {
+    color.breaks <- pretty(
+      c(0,
+        seq(from = min(0, na.rm = TRUE),
+            to = max(data[,.N, by = c('locationID', 'common_name', 'date')]$N, na.rm = TRUE))))
+  }
+
+  # If comparable.color.breaks == FALSE
+  # Create heatmap color breaks based only on this species, so that patterns in this species
+  # are easier to see
+  if (comparable.color.breaks == FALSE) {
+    color.breaks <- pretty(
+      c(0,
+        seq(from = min(0, na.rm = TRUE),
+            to = max(data[common_name == common.name & confidence >= conf.threshold & locationID == locid,
+                          .N,
+                          by = c('locationID', 'common_name', 'date')]$N, na.rm = TRUE))))
+  }
+
   # Subset data according to input params
   dts <- data[common_name == common.name
               & confidence >= conf.threshold
@@ -128,10 +175,8 @@ birdnet_heatmap <- function(
   dtn[,year :=  year(date)]
   dtn[,julian.date := yday(date)]
 
-  # Set reasonable color breaks
+  # Set colors
   heat.cols <- c('gray75', viridis::magma(n = 5, begin = 0.2))
-  mx <- max(dtn$N, na.rm = TRUE)
-  color.breaks <- pretty(c(0, seq(from = 1, to = mx, by = mx/5)))
   u.lim <- max(color.breaks)
 
   dtn <- dtn[!is.na(year)]
@@ -142,7 +187,7 @@ birdnet_heatmap <- function(
                   fill = N)) +
     geom_tile(color = 'black', alpha = 1, width = 1) +
     scale_fill_gradientn(
-      limits  = range(0, u.lim),
+      limits  = range(color.breaks),
       colors = heat.cols,
       breaks = color.breaks
     ) +
